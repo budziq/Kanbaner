227b1902607d5e2d9976a3d8d34b82981e00fb93
Michał Budzyński <m.budzynski@samsung.com>
2012-10-31 00:49:12 +0100

    reformatted bad indentation

--- src/com/budziq/kanbaner/BoardListFragment.java
+++ src/com/budziq/kanbaner/BoardListFragment.java
@@ -10,12 +10,12 @@ import com.actionbarsherlock.app.SherlockFragment;
 
 public class BoardListFragment extends SherlockFragment {
 
-	private String mContent;
-	private static final String KEY_TAB_MSG = "key.tab.msg";
+    private String mContent;
+    private static final String KEY_TAB_MSG = "key.tab.msg";
 
-	public static BoardListFragment newInstance(String text) {
-		BoardListFragment fragment = new BoardListFragment();
-        
+    public static BoardListFragment newInstance(String text) {
+        BoardListFragment fragment = new BoardListFragment();
+
         // Supply num input as an argument.
         Bundle args = new Bundle();
         args.putString(KEY_TAB_MSG, text);
@@ -23,24 +23,24 @@ public class BoardListFragment extends SherlockFragment {
 
         return fragment;
     }
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState){
 
-		View v = inflater.inflate(R.layout.board_list, null);
-		TextView tv = (TextView) v.findViewById(R.id.textView1);
-		
-		String msg = getString(R.string.hello_world) + mContent;
-		tv.setText(msg);
-		return v;
-		
-	};
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mContent =  getArguments() != null ? getArguments().getString(KEY_TAB_MSG) : "???";
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState){
+
+        View v = inflater.inflate(R.layout.board_list, null);
+        TextView tv = (TextView) v.findViewById(R.id.textView1);
+
+        String msg = getString(R.string.hello_world) + mContent;
+        tv.setText(msg);
+        return v;
+
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContent =  getArguments() != null ? getArguments().getString(KEY_TAB_MSG) : "???";
+    }
 
 }
